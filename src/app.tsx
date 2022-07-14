01c746d07a0ab2367abf1c7936c03266a23c0f2b
Ayeman <ayemanbsalauddin@gmail.com>
2022-07-14 16:45:19 +0600

    First Commit ☕☕

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, hic eos
+      deleniti debitis quasi, esse magnam architecto tenetur excepturi est
+      officia impedit? Praesentium repellendus expedita natus quae vitae,
+      perferendis provident.
+    </div>
+  );
+}
